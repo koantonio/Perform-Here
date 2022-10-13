25b52a7ef8388b3fe4984cd9b675eaef72002e5d
Sai <sdk128@scarletmail.rutgers.edu>
2022-10-13 13:22:45 -0400

    fixed errors on refresh

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Artist } from '../artist';
 import { ArtistsService } from '../artists.service';
 import { CognitoService } from '../cognito.service';
-
 import { UserService } from '../user.service';
+import { Votes } from '../votes';
+import { VotesService } from '../votes.service';
+
 var name:string;
 @Component({
   selector: 'app-profile',
@@ -13,13 +15,13 @@ var name:string;
 export class ProfileComponent implements OnInit {
   artists : Artist[]= [];
   newArtists: Artist[] = [];
-  userId: string = "";
+  userId: string | null = "";
   isArtist: boolean = false;
   a: Artist = new Artist("", "", "");
-
+  votesByUser: Votes[] = [];
 
   //initializing artist and newArtist arrays
-  constructor(private artistService: ArtistsService, private userService: UserService, private cognitoService: CognitoService) {
+  constructor(private votesService:VotesService, private artistService: ArtistsService, private userService: UserService, private cognitoService: CognitoService) {
     this.artistService.getAllArtists().subscribe(artists=>{
       this.artists=artists;
       this.newArtists = artists;
@@ -30,26 +32,19 @@ export class ProfileComponent implements OnInit {
 
   //grabs users email
   ngOnInit(): void {
-
     this.userId = this.cognitoService.getEmail();
-    console.log(this.userId);
     this.artistService.getArtistById(this.userId).subscribe(artist => {
       console.log(artist);//prints out current logins email
-      
-      if(artist == null) {
-
-      }
-      else {
+      if(artist != null) {
         this.isArtist = true;
         this.a=artist;
       }
     });
-   
+  this.votesService.getAllVotes().subscribe(votes => this.votesByUser = votes);
+  console.log(this.votesByUser);
   }
 
   updateArtist() {
-  this.artistService.addArtist(this.a).subscribe( res=> console.log(res));
+    this.artistService.addArtist(this.a).subscribe(res => console.log(res));
   }
-
-
 }
---
+++ src/app/votes.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { CognitoService } from './cognito.service';
+import { Votes } from './votes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VotesService {
+
+  baseUrl: string = "http://960169performhereapi-env.eba-yairqaej.us-west-2.elasticbeanstalk.com/votes/";
+  postHeaders = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+
+  constructor(private http:HttpClient, private cognitoService:CognitoService) { }
+
+  getVotesByUser(): Observable<Votes[]> {
+    let userId = this.cognitoService.getEmail();
+    return this.http.get<Votes[]>(this.baseUrl+userId)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  getAllVotes(): Observable<Votes[]> {
+    return this.http.get<Votes[]>(this.baseUrl+"all")
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  addVote(v: Votes) {
+    const body = {
+      "id": v.id,
+      "userId": v.userId,
+      "artistName": v.artistName,
+      "state": v.state,
+      "city": v.city
+    }
+    return this.http.post(this.baseUrl+"add", body, this.postHeaders)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
