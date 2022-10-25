277af92e081e7d591c85f9acb6c307ed483ae9d0
Campbell <960148@cognizant.com>
2022-10-25 17:29:50 -0500

    More Tests

--- src/app/stats-table/stats-table.component.spec.ts
+++ src/app/stats-table/stats-table.component.spec.ts
@@ -1,16 +1,37 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StatsTableComponent } from './stats-table.component';
+import { CityVotes } from './stats-table.component';
+import { Votes } from '../votes';
+import { VotesService } from '../votes.service';
 
 describe('StatsTableComponent', () => {
   let component: StatsTableComponent;
   let fixture: ComponentFixture<StatsTableComponent>;
+  let votesServiceSpy: jasmine.SpyObj<VotesService>;
+
+  let expectedVotesPerCity: CityVotes[] = [
+    new CityVotes(2, 'testCity', 'testState', 'testStage'),
+    new CityVotes(1, 'City', 'State', 'testStage')
+  ];
+
+  let inputVotes: Votes[] = [
+    new Votes('test@test', 'testStage', 'testState', 'testCity', 'testDiscount'),
+    new Votes('test@test', 'testStage', 'testState', 'testCity', 'testDiscount'),
+    new Votes('test@test', 'testStage', 'State', 'City', 'testDiscount'),
+  ];
+
+  let discountCode: HTMLInputElement;
 
   beforeEach(async () => {
+    votesServiceSpy = jasmine.createSpyObj('VotesService',['updateVotes']);
+
     await TestBed.configureTestingModule({
       declarations: [ StatsTableComponent ],
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [
+        { provide: VotesService, useValue: votesServiceSpy }
+      ]
     })
     .compileComponents();
 
@@ -21,5 +42,11 @@ describe('StatsTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('calculateVotes should return a sorted array of CityVotes', () => {
+    component.votes = inputVotes;
+    component.calculateVotes();
+    expect(expectedVotesPerCity).toEqual(expectedVotesPerCity);
   });
 });
